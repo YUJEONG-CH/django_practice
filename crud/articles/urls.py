7154253be8e3210371a281b0d0d2dea64423c263
yujeong6676 <stellachoi6676@gmail.com>
2022-10-25 16:38:32 +0900

    06 - Article model

--- crud/articles/urls.py
+++ crud/articles/urls.py
@@ -1,8 +1,10 @@
 # URL 설정을 app단위로 !
 from django.urls import path
+from . import views
 
 app_name = 'articles'
 urlpatterns = [
-
+    #http://localhost:8000/articles
+    path('', views.index, name='index'),
 
 ]
