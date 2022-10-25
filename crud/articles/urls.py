e048f3ca42437c32b5387a207ae95e51dfb60979
yujeong6676 <stellachoi6676@gmail.com>
2022-10-25 17:09:53 +0900

    07 - 게시글 작성하기

--- crud/articles/urls.py
+++ crud/articles/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 app_name = 'articles'
 urlpatterns = [
-    #http://localhost:8000/articles
+    # http://localhost:8000/articles
     path('', views.index, name='index'),
+    # http://localhost:8000/articles/new
+    path('new/', views.new, name='new'),
+    # http://localhost:8000/articles/create
+    path('create/', views.create, name='create'),
 
 ]
