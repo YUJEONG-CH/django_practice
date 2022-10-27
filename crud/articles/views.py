4b918c5349f6293efae1affc793da43ca8a34ee2
yujeong6676 <stellachoi6676@gmail.com>
2022-10-27 15:51:12 +0900

    08-게시글 목록

--- crud/articles/views.py
+++ crud/articles/views.py
@@ -5,9 +5,14 @@ from .models import Article
 
 # 요청 정보를 받아서...
 def index(request):
-
+    # 게시글을 가져와서..
+    articles = Article.objects.order_by('-pk')# 역순 - queryset(Article객체를 가진)
+    # Template에 전달한다..
+    context = {
+        'articles': articles
+    }
     # 페이지를 render...
-    return render(request, 'articles/index.html')
+    return render(request, 'articles/index.html', context)
 
 def new(request):
     return render(request, 'articles/new.html')
