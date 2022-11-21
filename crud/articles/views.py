3a3eab70a7256c2d6cd3205e56198fdb5664cec0
yujeong6676 <stellachoi6676@gmail.com>
2022-11-21 18:32:24 +0900

    end

--- crud/articles/views.py
+++ crud/articles/views.py
@@ -48,10 +48,20 @@ def detail(request, pk):
     return render(request, 'articles/detail.html', context)
 
 def update(request, pk):
-    # GET : Form을 제공
     article = Article.objects.get(pk=pk)
-    article_form = ArticleForm(instance=article)
-    context = {
-        'article_form': article_form
-    }
-    return render(request, 'articles/update.html', context)
+    if request.method =='POST':
+        #POST : input 값 가져와서, 검증하고, DB에 저장
+        article_form = ArticleForm(request.POST, instance=article)
+        if article_form.is_valid():
+            #유효성 검사 통과하면 저장하고, 상세보기 페이지로
+            article_form.save()
+            return redirect('articles:detail', article.pk)
+        #유효성 검사 통과하지 않으면 > context부터해서 오류메시지 담긴 article_form 렌더링
+    else:
+        # GET : Form을 제공
+        
+        article_form = ArticleForm(instance=article)
+        context = {
+            'article_form': article_form
+        }
+        return render(request, 'articles/update.html', context)
